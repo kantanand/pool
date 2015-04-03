40481d1f120e8e1b488963467db458d9bc18af2a
Kantanand US <kantanand@users.noreply.github.com>
2015-04-03 20:24:44 +0530

    android pool project created

---
+++ android/pool/src/com/mobisir/pool/PoolHome.java
@@ -0,0 +1,15 @@
+package com.mobisir.pool;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PoolHome extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
