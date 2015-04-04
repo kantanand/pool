33721dd282e5225067155d9cd38fb4b015fd3d8b
Kantanand US <kantanand@users.noreply.github.com>
2015-04-04 12:09:56 +0530

    app icon added

--- android/pool/src/com/mobisir/pool/PoolHome.java
+++ android/pool/src/com/mobisir/pool/PoolHome.java
@@ -2,6 +2,7 @@ package com.mobisir.pool;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.content.Intent;
 
 public class PoolHome extends Activity
 {
@@ -11,5 +12,18 @@ public class PoolHome extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        getActionBar().hide();
+        new Thread() {
+            public void run() {
+                try {
+                    	Intent i = new Intent(PoolHome.this,PoolSearch.class);
+                        Thread.sleep(1500);
+                        startActivity(i);
+                        finish();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+        }.start();
     }
 }
