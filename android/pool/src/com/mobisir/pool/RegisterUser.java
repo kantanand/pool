604160bb2593ffc99b93854654bdbe73c83cade5
Kantanand US <kantanand@users.noreply.github.com>
2015-04-04 21:03:45 +0530

    Registration page added and on click of join navigatin to poolsearch

--- android/pool/src/com/mobisir/pool/RegisterUser.java
+++ android/pool/src/com/mobisir/pool/RegisterUser.java
@@ -3,6 +3,9 @@ package com.mobisir.pool;
 import android.app.Activity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class RegisterUser extends Activity
 {
@@ -11,6 +14,14 @@ public class RegisterUser extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.registration);
+        setContentView(R.layout.registration); 
+        final Button join_user = (Button) findViewById(R.id.join_btn);
+        join_user.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent i = new Intent(RegisterUser.this, PoolSearch.class);
+                startActivity(i);
+            }
+        });
     }
 }
