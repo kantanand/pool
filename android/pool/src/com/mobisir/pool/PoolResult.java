82f32e7afe2483741b55abfd446caf9d4d6d8b90
Kantanand US <kantanand@users.noreply.github.com>
2015-04-08 23:20:30 +0530

    Java Files Created for each pages

---
+++ android/pool/src/com/mobisir/pool/PoolResult.java
@@ -0,0 +1,16 @@
+package com.mobisir.pool;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+
+public class PoolResult extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pool_search);
+    }
+}
