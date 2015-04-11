86fecd6081679530b6cabd806593099487ff4fb1
mvpnitesh <parimalanitesh@gmail.com>
2015-04-12 02:53:21 +0530

    Changes of POOL DB

--- db/pooldb.sql
+++ db/pooldb.sql
@@ -5,17 +5,18 @@ grant all on pooldb.* to 'pooladmin'@'localhost' identified by 'pool@123#';
 
 use pooldb;
 
-create table if not exists `pool_users` (
+create table if not exists pool_users (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     user_id varchar(40) NOT NULL,
     age int DEFAULT '0' NOT NULL,
     gender varchar(20) NOT NULL,
     mobile varchar(20) ,
-    location varchar(30) ,
+    location varchar(30),
+    unique (id),
     PRIMARY KEY (user_id)
 )ENGINE=INNODB CHARSET=utf8;
 
-create table if not exists `pool_tag_master` (
+create table if not exists pool_tag_master (
     pool_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     pool_tag varchar(100) NOT NULL,
 	question text NOT NULL,
@@ -29,15 +30,16 @@ create table if not exists `pool_tag_master` (
     pool_start timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
 	pool_end timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
 	pool_tag_created timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	option1_count INT UNSINGNED NOT NULL DEFAULT '0',
-	option2_count INT UNSINGNED NOT NULL DEFAULT '0',
-	option3_count INT UNSINGNED NOT NULL DEFAULT '0',
-	option4_count INT UNSINGNED NOT NULL DEFAULT '0',
+	option1_count INT UNSIGNED NOT NULL DEFAULT 0,
+	option2_count INT UNSIGNED NOT NULL DEFAULT 0,
+	option3_count INT UNSIGNED NOT NULL DEFAULT 0,
+	option4_count INT UNSIGNED NOT NULL DEFAULT 0,
+    unique (pool_id),
     PRIMARY KEY (pool_tag),
 	CONSTRAINT pool_tag_user FOREIGN KEY (user_id) REFERENCES pool_users(user_id)
 )ENGINE=INNODB CHARSET=utf8;
 
-create table if not exists 'pool_tag_answer'(
+create table if not exists pool_tag_answer(
 	answer_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     pool_tag varchar(100) NOT NULL,
 	option_selected varchar(10) NOT NULL,
@@ -47,6 +49,6 @@ create table if not exists 'pool_tag_answer'(
     gender varchar(20) NOT NULL,
 	pool_tag_answered timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
    	PRIMARY KEY (answer_id),
-	CONSTRAINT pool_tag_user FOREIGN KEY (user_id) REFERENCES pool_users(user_id),
+	CONSTRAINT pool_tag_user_answered FOREIGN KEY (user_id) REFERENCES pool_users(user_id),
 	CONSTRAINT pool_tag_question FOREIGN KEY (pool_tag) REFERENCES pool_tag_master(pool_tag)
 )ENGINE=INNODB CHARSET=utf8;
