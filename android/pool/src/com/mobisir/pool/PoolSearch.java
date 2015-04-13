bcac7ee07eeb081072635951331c6b5d7b7b26ba
Kantanand US <kantanand@users.noreply.github.com>
2015-04-14 01:49:55 +0530

    Contact.java class for getter and setter method for DB details and DatabasseHandeler file added

--- android/pool/src/com/mobisir/pool/PoolSearch.java
+++ android/pool/src/com/mobisir/pool/PoolSearch.java
@@ -1,8 +1,12 @@
 package com.mobisir.pool;
+import com.mobisir.pool.DatabaseHandler;
+import java.util.List;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.widget.TextView;
+import android.util.Log;
 
 public class PoolSearch extends Activity
 {
@@ -12,5 +16,26 @@ public class PoolSearch extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pool_search);
+        DatabaseHandler db = new DatabaseHandler(this);
+         
+        /**
+         * CRUD Operations
+         * */
+        // Inserting Contacts
+        Log.d("Insert: ", "Inserting .."); 
+        db.addContact(new Contact("Ravi", "9100000000"));        
+        db.addContact(new Contact("Srinivas", "9199999999"));
+        db.addContact(new Contact("Tommy", "9522222222"));
+        db.addContact(new Contact("Karthik", "9533333333"));
+         
+        // Reading all contacts
+        Log.d("Reading: ", "Reading all contacts.."); 
+        List<Contact> contacts = db.getAllContacts();       
+         
+        for (Contact cn : contacts) {
+            String log = "Id: "+cn.getID()+" ,Name: " + cn.getName() + " ,Phone: " + cn.getPhoneNumber();
+            // Writing Contacts to log
+        	Log.d("Name: ", log);
+    	}
     }
 }
